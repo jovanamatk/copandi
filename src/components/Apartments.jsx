79b919dec133c5febec0e66a5cc38afd2c4d87f9
Jovana Matkovic <mat.jovana@gmail.com>
2020-10-15 01:17:21 +0200

    Added apartment component, generated an object that holds information about apartments, rendered apartments

--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Apartment from './child/Apartment'
+import {getApartments} from '../services/apartmentsService'
 
 const Apartments = () => {
-    return ( <> </> );
+    let [apartments, setApartments] = useState([]);
+
+    useEffect(() => {
+        const aps = [...getApartments()]
+        setApartments(aps);
+    }, []);
+
+    return ( 
+    <div className="container-fluid">
+        <div className="row top-line">
+  <div className="row w-80 mx-auto p-3 apartments-grid">
+    {apartments.map(apartment=><div key={apartment._id} className="col-lg-4 col-md-6"><Apartment key={apartment._id} apartment={apartment}/></div>)}
+  </div>
+</div>
+</div>
+     );
 }
  
 export default Apartments;
---
+++ src/components/child/Apartment.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+const Apartment = ({apartment}) => {
+    return ( <> 
+
+    <img className="rounded w-100 img apartment-img" alt={apartment.description} src={apartment.image}/>
+    <div className="row">
+    <div className="mx-auto apartment-box p-3">
+    <p className="apartment-description">{apartment.description}</p>
+
+    <p className="font-weight-bolder location-styling font-small opacity-03"><FontAwesomeIcon icon={faMapMarkerAlt}   /> <span>{apartment.location}</span></p>
+
+    <div className="row details">
+    <div className="col-4 detail">
+            <p className="font-small"><span className="opacity-05">Vierkante meters</span><br/>
+                {apartment.sqmtr}
+            </p>
+        </div>        
+        <div className="col-4 detail">
+        <p className="font-small"><span className="opacity-05">Slaapkamers</span><br/>
+                {apartment.rooms}
+            </p>
+        </div>        
+        <div className="col-4 detail">
+            <p className="font-small"><span className="opacity-05">Plaats</span><br/>
+                <span className="price">{apartment.price}</span>
+            </p>
+        </div>
+    </div>
+    </div>
+    </div>
+
+    </> );
+}
+ 
+export default Apartment;
