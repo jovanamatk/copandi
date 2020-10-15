8f791152dc4eadb1b41513be4a38f4f7a01f2d84
Jovana Matkovic <mat.jovana@gmail.com>
2020-10-15 02:14:59 +0200

    Used js instead of JSON for apartments, so I've modified it to be a JSON file

--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,20 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import Apartment from './child/Apartment'
-import {getApartments} from '../services/apartmentsService'
+import apartmentsJSON from  '../services/apartmentsService.json'
 
 const Apartments = () => {
     let [apartments, setApartments] = useState([]);
 
     useEffect(() => {
-        const aps = [...getApartments()]
-        setApartments(aps);
+        setApartments(apartmentsJSON);
     }, []);
+    
 
     return ( 
     <div className="container-fluid">
         <div className="row top-line">
   <div className="row w-80 mx-auto p-3 apartments-grid">
-    {apartments.map(apartment=><div key={apartment._id} className="col-lg-4 col-md-6"><Apartment key={apartment._id} apartment={apartment}/></div>)}
+    {apartments.length <= 0 ? <h1 className="text-white">There are no apartments.</h1> : apartments.map(apartment=><div key={apartment._id} className="col-lg-4 col-md-6"><Apartment key={apartment._id} apartment={apartment}/></div>) }
   </div>
 </div>
 </div>
